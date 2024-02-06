1a6fdcf1a3d87904f2cdae650321a68d62e03ed1
lulu-cao <lulu.cao.her@gmail.com>
2024-02-06 19:42:45 +0800

    added MapView and updated ContentView

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    Text("California")
+                }
                     .font(.subheadline)
-                Spacer()
-                Text("California")
+                    .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("That's a rainy day when we first made our step")
             }
-        }
-        .padding()
+            .padding()
             
+            Spacer()
+        }
     }
 }
 
